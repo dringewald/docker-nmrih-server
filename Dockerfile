8a91a11c52bc510d22100d2a4ec939d91977c98f
Dustin Ringewald <dringewald@nox-gaming.de>
2022-03-05 22:58:14 +0100

    Added startgame.sh startscript

--- Dockerfile
+++ Dockerfile
@@ -34,6 +34,7 @@ RUN mkdir /opt/nmrih
 # Copy files
 COPY files/entrypoint.sh /
 COPY files/nmrih-setup.sh /opt/nmrih/nmrih-setup.sh
+COPY files/stargame.sh /opt/nmrih/stargame.sh
 COPY files/keyfiles/id_ed25519.pub /etc/ssh/keyfiles/id_ed25519.pub
 COPY files/ssh/sshd_config /etc/ssh/sshd_config
 
---
+++ files/startgame.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Check if .steam-Folder exists
+if [ ! -d /home/nmrih/.steam ]; then
+	mkdir -vp /home/nmrih/.steam
+fi
+
+# Check if Symlink exists
+if [ ! -L /home/nmrih/.steam/sdk32 ]; then
+	ln -s /home/nmrih/steamcmd/linux32 /home/nmrih/.steam/sdk32
+fi
+
+# Check if Variables are empty
+if [ -z "$NMRIH_NET_PUBLIC_ADDRESS" ]; then
+	export NMRIH_NET_PUBLIC_ADDRESS="0.0.0.0"
+fi
+
+if [ -z "$NMRIH_CLIENT_PORT" ]; then
+	export NMRIH_CLIENT_PORT="27010"
+fi
+
+if [ -z "$NMRIH_PORT" ]; then
+	export NMRIH_PORT="27015"
+fi
+
+if [ -z "$NMRIH_TV_PORT" ]; then
+	export NMRIH_TV_PORT="27020"
+fi
+
+if [ -z "$NMRIH_STARTMAP" ]; then
+	export NMRIH_STARTMAP="nmo_cabin"
+fi
+
+if [ -z "$NMRIH_CONFIG_FILE" ]; then
+	export NMRIH_CONFIG_FILE="server.cfg"
+fi
+
+if [ -z "$NMRIH_MAXPLAYERS" ]; then
+	export NMRIH_MAXPLAYERS="8"
+fi
+
+if [ -z "$NMRIH_REGION" ]; then
+	export NMRIH_REGION="3"
+fi
+
+if [ -z "$NMRIH_NET_PUBLIC_ADDRESS" ]; then
+	export NMRIH_NET_PUBLIC_ADDRESS="0.0.0.0"
+fi
+
+echo "Start up the Server"
+echo "-------------------"
+sudo -i -u nmrih /home/nmrih/server/srcds_run \
+	-game nmrih \
+	-insecure \
+	-strictportbind \
+	-ip $NMRIH_NET_PUBLIC_ADDRESS \
+	-port $NMRIH_PORT \
+	+clientport $NMRIH_CLIENT_PORT \
+	+tv_port $NMRIH_TV_PORT \
+	+map $NMRIH_STARTMAP \
+	+servercfgfile $NMRIH_CONFIG_FILE \
+	-maxplayers $NMRIH_MAXPLAYERS \
+	+sv_region $NMRIH_REGION \
+	+net_public_adr $NMRIH_NET_PUBLIC_ADDRESS \
+	-authkey $NMRIH_AUTH_KEY \
+	+sv_setsteamaccount $NMRIH_TOKEN \
+	$NMRIH_ADDITIONAL_ARGS
